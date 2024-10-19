42d0bd28c716224453a193def012e048d2f7ee38
Eugene Tkachenko <gektor650@gmail.com>
2024-10-18 21:56:55 -0600

    React on messages

--- main/app_main.c
+++ main/app_main.c
@@ -13,9 +13,11 @@
 
 static const char *TAG = "MAIN";
 
-#define RELAY_PIN (gpio_num_t) GPIO_NUM_4
+#define RELAY_PIN (gpio_num_t) GPIO_NUM_33
 #define MESSAGE_ON "BOO!"
 
+static const TickType_t MESSAGE_COOLDOWN_TICKS = pdMS_TO_TICKS(10000);
+static TickType_t last_message_time = 0;
 int port = 9341;
 
 /**
@@ -24,13 +26,24 @@ int port = 9341;
 static void nerdy_udp_server_event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) 
 {
     if (event_base != NERDY_UDP_SERVER_EVENT) return;
+    
     if (event_id == NERDY_UDP_SERVER_EVENT_MESSAGE) {
+        // Get the current time in ticks
+        TickType_t current_time = xTaskGetTickCount();
+
+        // Check if the cooldown period has passed
+        if ((current_time - last_message_time) < MESSAGE_COOLDOWN_TICKS) {
+            ESP_LOGI(TAG, "Ignoring message, still in cooldown period.");
+            return; // Skip processing this message
+        }
+        last_message_time = current_time;
+
         char *message = (char*)event_data;
         ESP_LOGI(TAG, "Received in main: %s", message);
         if (strncmp(message, MESSAGE_ON, strlen(MESSAGE_ON)) == 0) {
             gpio_set_level(RELAY_PIN, 1);
             ESP_LOGI(TAG, "Turning ON");
-            vTaskDelay(1000 / portTICK_PERIOD_MS);
+            vTaskDelay(500 / portTICK_PERIOD_MS);
             gpio_set_level(RELAY_PIN, 0);
             ESP_LOGI(TAG, "Turning OFF");
         }
